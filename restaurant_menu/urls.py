61c4da6b63f291d9a700eb0abbc624c77b9d0e79
Rajat <rajatdoit18@gmail.com>
2024-07-18 21:49:30 +0530

    Added code for the detail page of each dish

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MenuList
+from .views import MenuList, MenuItemDetail
 
 urlpatterns = [
     path('', MenuList.as_view(), name='home'),
+    path('item/<int:pk>/', MenuItemDetail.as_view(), name='menu_item')
 ]
